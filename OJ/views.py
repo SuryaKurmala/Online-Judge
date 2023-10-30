de02aaf446cc992623e6f07f00742b0ed0c383cd
Surya <ksmsurya2000@gmail.com>
2023-10-30 15:15:11 +0530

    Updated 3010

--- OJ/views.py
+++ OJ/views.py
@@ -37,29 +37,37 @@ def descriptionPage(request, problem_id):
 
 @login_required(login_url='login')
 def verdictPage(request, problem_id):
-     if request.method == 'POST':
-        problem = Problem.objects.get(id = problem_id)
-        testcase = TestCase.objects.get(problem_id = problem_id)
-        verdict, run_time  = "Wrong Answer", 0
+    if request.method == 'POST':
+        docker_client = docker.from_env()
+        Running = "running"
 
-        language = request.POST.get('language', '')
-        user_code = request.POST.get('user_code', '')
-        user_code = user_code.replace('\r\n', '\n').strip()
-        print(f"Language: {language}")
-        print(f"User Code: {user_code}")
+        problem = Problem.objects.get(id=problem_id)
+        testcase = TestCase.objects.get(problem_id=problem_id)
+        testcase.output = testcase.output.replace('\r\n','\n').strip() 
+
+        verdict = "Wrong Answer" 
+        res = ""
+        run_time = 0
+
+        form = CodeForm(request.POST)
+        user_code = ''
+        if form.is_valid():
+            user_code = form.cleaned_data.get('user_code')
+            user_code = user_code.replace('\r\n','\n').strip()
+            
+        language = request.POST['language']
         submission = Submission(user=request.user, problem=problem, submission_time=datetime.now(), 
                                     language=language, user_code=user_code)
         submission.save()
-        filename = str(submission.id)
 
-        testcase.output = testcase.output.replace('\r\n','\n').strip() 
+        filename = str(submission.id)
 
         if language == "C++":
             extension = ".cpp"
             cont_name = "oj-cpp"
             compile = f"g++ -o {filename} {filename}.cpp"
             clean = f"{filename} {filename}.cpp"
-            docker_img = "gcc"
+            docker_img = "gcc:11.2.0"
             exe = f"./{filename}"
             
         elif language == "C":
@@ -67,7 +75,7 @@ def verdictPage(request, problem_id):
             cont_name = "oj-c"
             compile = f"gcc -o {filename} {filename}.c"
             clean = f"{filename} {filename}.c"
-            docker_img = "gcc"
+            docker_img = "gcc:11.2.0"
             exe = f"./{filename}"
 
         elif language == "Python":
@@ -77,7 +85,7 @@ def verdictPage(request, problem_id):
             clean = f"{filename}.py"
             docker_img = "python"
             exe = f"python {filename}.py"
-        
+
         elif language == "Java":
             filename = "Main"
             extension = ".java"
@@ -86,27 +94,27 @@ def verdictPage(request, problem_id):
             clean = f"{filename}.java {filename}.class"
             docker_img = "openjdk"
             exe = f"java {filename}"
-        
-        file = filename + extension
-        filepath = os.path.join(settings.FILES_DIR, file)
 
-        os.makedirs(os.path.dirname(filepath), exist_ok=True)
-        with open(filepath, "w") as code:
-            code.write(user_code)
-        print("Local File Path: ", filepath)
-        
+
+        file = filename + extension
+        filepath = settings.FILES_DIR + "/" + file
+        code = open(filepath,"w")
+        code.write(user_code)
+        code.close()
+
         try:
-            docker_client = docker.from_env()
             container = docker_client.containers.get(cont_name)
             container_state = container.attrs['State']
-            if not container_state['Status'] == "running":
+            container_is_running = (container_state['Status'] == Running)
+            if not container_is_running:
                 subprocess.run(f"docker start {cont_name}",shell=True)
-
         except docker.errors.NotFound:
             subprocess.run(f"docker run -dt --name {cont_name} {docker_img}",shell=True)
+
+
         subprocess.run(f"docker cp {filepath} {cont_name}:/{file}",shell=True)
+
         cmp = subprocess.run(f"docker exec {cont_name} {compile}", capture_output=True, shell=True)
-        print(cmp)
         if cmp.returncode != 0:
             verdict = "Compilation Error"
             subprocess.run(f"docker exec {cont_name} rm {file}",shell=True)
@@ -118,13 +126,13 @@ def verdictPage(request, problem_id):
                                                 capture_output=True, timeout=1, shell=False)
                 run_time = time()-start
                 subprocess.run(f"docker exec {cont_name} rm {clean}",shell=True)
-                print(res)
             except subprocess.TimeoutExpired:
                 run_time = time()-start
                 verdict = "Time Limit Exceeded"
                 subprocess.run(f"docker container kill {cont_name}", shell=True)
                 subprocess.run(f"docker start {cont_name}",shell=True)
                 subprocess.run(f"docker exec {cont_name} rm {clean}",shell=True)
+
 
             if verdict != "Time Limit Exceeded" and res.returncode != 0:
                 verdict = "Runtime Error"
@@ -137,9 +145,10 @@ def verdictPage(request, problem_id):
         
         elif verdict == "Wrong Answer":
             user_stdout = res.stdout.decode('utf-8')
-            if user_stdout == testcase.output:
+            if str(user_stdout)==str(testcase.output):
                 verdict = "Accepted"
-            if user_stdout == testcase.output + '\n':
+            testcase.output += '\n'
+            if str(user_stdout)==str(testcase.output):
                 verdict = "Accepted"
 
         submission.verdict = verdict
@@ -148,9 +157,5 @@ def verdictPage(request, problem_id):
         submission.run_time = run_time
         submission.save()
         os.remove(filepath)
-
-        context = {
-            'verdict': verdict,
-            'verdict_css_class': 'accepted' if verdict == 'Accepted' else 'wrong-answer' if verdict == 'Wrong Answer' else 'other'
-        }
+        context={'verdict':verdict}
         return render(request,'OJ/verdict.html',context)
